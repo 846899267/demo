039aa3eba6cd034993289d711863014f9eaf099b
846899267 <846899267@qq.com>
2020-08-05 16:06:01 +0800

    添加类和语句

---
+++ src/main/java/com/example/demo/a/a.java
@@ -0,0 +1,4 @@
+package com.example.demo.a;
+
+public class a {
+}
--- src/main/java/com/example/demo/a/cba.java
+++ src/main/java/com/example/demo/a/cba.java
@@ -1,5 +1,7 @@
 package com.example.demo.a;
 
 public class cba {
-
+    public static void main(String[] args) {
+        System.out.println("a");
+    }
 }
